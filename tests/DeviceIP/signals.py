0d331cbdeb63fb26ac9a7f3bb87977e5ed7c3398
joaoalbuquerque <joao.albuquerque@elcoma.com.br>
2020-03-03 18:59:10 -0300

    updating suggestions

--- tests/DeviceIP/signals.py
+++ tests/DeviceIP/signals.py
@@ -10,25 +10,23 @@ from openwisp_ipam.models import IpAddress, Subnet
 def device_saved_receiver(sender, instance, created, **kwargs):
         
     if not created:
-        #track changes to deviceip
-            
-        print("not created")
+        
+	#track manual changes to deviceip (usefull to avoid conflict with non-openwisp-devices such as gateways, dns and other servers on same subnet) 
            
     if created:
             
-        #request new ip based on org subnet
+        #request new ip based on org subnet (retrieve org and subnet first)
             
-        #print("req_ip: ",req_ip)
-            #request new ip based on org subnet
+        #request new ip based on org subnet (using request.get and subnet API) 
 
-            #save ip to device config
+        #save ip to device config (either setting context variable or editing device network interface with same same as subnet)
 
-            #update subnet
+        #save changes to the subnet  
             
     pass
 
 @receiver(post_delete, sender=Device)
 def device_deleted_receiver(sender, instance, **kwargs):
-   # may want to do something on delete
+    #Make device's IP available again, update orgnization subnet 
     pass
 
